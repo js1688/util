94e9c1f00c13830d571c612d5258003adccebba9
js1688 <woshitanjun@icloud.com>
2019-04-02 17:21:02 +0800

    增加勾选功能,三种勾选状态
    增加勾选功能,三种勾选状态

--- treegrid/jquery.treegrid.extension.js
+++ treegrid/jquery.treegrid.extension.js
@@ -1,25 +1,102 @@
-        function treeGridClickFun(ts,id,type){//true 将触发展开,false 不触发展开
-            var trs = $(ts).parent().parent().nextAll('.treegrid-parent-'+id);
-            for(var l = 0; l < trs.length; l++){
-            var lcss = $(trs[l]).css('display');
-                if(lcss == 'none' && type){
-                  $(trs[l]).css('display','');
-                }else{
-                  $(trs[l]).css('display','none');
-                  var nodeId = $(trs[l]).attr('nodeId');
-                  var sanjiao = $(trs[l]).find('.treegrid-expander-expanded');
-                  if(sanjiao.length > 0){
-                    $(sanjiao[0]).removeClass('treegrid-expander-expanded',);
-                    $(sanjiao[0]).addClass('treegrid-expander-collapsed');
-                  }
-                  treeGridClickFun(ts,nodeId,false);
-                }
+function treeGridClickFun(ts,id,type){//true 将触发展开,false 不触发展开
+    var trs = $(ts).parent().parent().nextAll('.treegrid-parent-'+id);
+    for(var l = 0; l < trs.length; l++){
+        var lcss = $(trs[l]).css('display');
+        if(lcss == 'none' && type){
+            $(trs[l]).css('display','');
+        }else{
+            $(trs[l]).css('display','none');
+            var nodeId = $(trs[l]).attr('nodeId');
+            var sanjiao = $(trs[l]).find('.treegrid-expander-expanded');
+            if(sanjiao.length > 0){
+                $(sanjiao[0]).removeClass('treegrid-expander-expanded',);
+                $(sanjiao[0]).addClass('treegrid-expander-collapsed');
             }
-            if(type){
-                $(ts).toggleClass('treegrid-expander-collapsed');
-                $(ts).toggleClass('treegrid-expander-expanded');
+            treeGridClickFun(ts,nodeId,false);
+        }
+    }
+    if(type){
+        $(ts).toggleClass('treegrid-expander-collapsed');
+        $(ts).toggleClass('treegrid-expander-expanded');
+    }
+};
+function treeGridCheckbox(ts,id) {//点击勾选框触发事件
+    //递归找到所有子节点设置为选中
+    var tschecked = $(ts).prop("checked");
+    var tsindeterminate = $(ts).prop("indeterminate");
+    function d(dtrs,dtschecked,tsindeterminate) {
+        for(var childObj = 0; childObj < dtrs.length; childObj++){
+            var obj = $(dtrs[childObj]);
+            var input = obj.find("input[type='checkbox']");
+            input.prop("checked",dtschecked);
+            input.prop("indeterminate",tsindeterminate);
+            var child = obj.attr("nodeId");
+            d2(input,child,dtschecked);
+        }
+    }
+    function d2(dts,did,d2tschecked,tsindeterminate) {
+        var trs = $(dts).parent().parent().nextAll('.treegrid-parent-'+did);
+        d(trs,d2tschecked,tsindeterminate);
+    }
+    d2(ts,id,tschecked,tsindeterminate);
+    treeGridCheckbox_s(ts);//检查父节点
+}
+
+function treeGridCheckbox_s(ts) {
+    //递归找父节点检查父节点所属的子节点是否为全部都勾选了,如果是则也设置勾选,如果为否则设置半勾选
+    function t(ttrs,tid) {
+        var input = $(ttrs).find("input[type='checkbox']");
+        var g = input.prop("checked");
+        var is = t3(ttrs,tid);
+        if(is == true){
+            input.prop("indeterminate", false);
+            input.prop("checked",true);
+        }else if(is == false){
+            input.prop("indeterminate", false);
+            input.prop("checked",g);
+        }else{
+            input.prop("indeterminate", true);
+            input.prop("checked",true);
+        }
+        t2(input);
+    }
+    function t2(tts){
+        var ttsObj = $(tts).parent().parent();
+        var trs = ttsObj.prevAll('.treegrid-'+ttsObj.attr("pId"));
+        if(trs.length == 0){
+            return;
+        }
+        var pid = $(trs[0]).attr("nodeId");
+        t(trs[0],pid);
+    }
+    function t3(ttrs,tid) {//true 全勾,false 全没有全勾 3 半选
+        //获取这个节点的所以子节点,检查是否都被设置为了勾选
+        var trs = $(ttrs).nextAll('.treegrid-parent-'+tid);
+        var tre = 0;
+        var fal = 0;
+        var bx = 0;
+        for(var i = 0; i < trs.length; i++){
+            var obj = $(trs[i]).find("input[type='checkbox']");
+            if(obj.prop("checked")){
+                tre++;
+            }else{
+                fal++;
+            }
+            if(obj.prop("indeterminate")){
+                bx++;
             }
         }
+        if(tre == trs.length && bx == 0){//全选
+            return true;
+        }else if(fal == trs.length && bx ==0){//全没被选中
+            return false;
+        }else{//半选
+            return 3;
+        }
+    }
+    t2(ts);
+}
+
 (function ($) {
     "use strict";
 
@@ -43,7 +120,10 @@
                 nowData[value[options.id]] = value;
             });
             return [result,nowData];
-        }; 
+        };
+        target.setIsSelected = function(selected){//可以再初始化之后在初始化之后再设置是否出现勾选框
+            options["selected"] = selected;
+        };
         //递归获取子节点并且设置子节点
         target.getChildNodes = function (data, parentNode, tbody,index) {
             index++;
@@ -53,12 +133,13 @@
                 $.each(childs, function (i, value) {//循环该节点所以子节点
                     var childNode = data[value];
                     if(childNode){
-                        var tr = $('<tr style="display:none;" name="child" nodeId="'+childNode[options.id]+'"></tr>');
+                        var tr = $('<tr style="display:none;" name="child" nodeId="'+childNode[options.id]+'" pId="'+parentNode[options.id]+'"></tr>');
                         tr.addClass('treegrid-' + childNode[options.id]);
                         tr.addClass('treegrid-parent-' + parentNode[options.id]);
                         tr.addClass("treegrid-expanded");
                         for (var j = 0; j < options.columns.length; j++) {
                             var td = $('<td></td>');
+                            var vl = childNode[options.columns[j].field];
                             if(j == 0){
                                 for(var k = 0; k < index; k++){
                                     td.append('<span class="treegrid-indent"></span>');
@@ -68,8 +149,16 @@
                                 }else{
                                     td.append('<span class="treegrid-indent"></span>');
                                 }
+                                if(options.selected){//如果开启了勾选,则生成单选框并赋予点击事件
+                                    var isselected = childNode["isselected"];
+                                    if(isselected){
+                                        target.selectedIds.push(childNode[options.id]);
+                                    }
+                                    var ckx = $('<input type="checkbox" onclick="treeGridCheckbox(this,\''+childNode[options.id]+'\')">');
+                                    td.append(ckx);
+                                }
                             }
-                            td.append(childNode[options.columns[j].field]);
+                            td.append(vl);
                             tr.append(td);
                         }
                         tbody.append(tr);
@@ -79,6 +168,11 @@
                 index++;
             }
         };
+        target.setSelected = function(id){//设置某个id为选中
+            var ts = target.find("tr[nodeId='"+id+"']").find("input[type='checkbox']");
+            $(ts).prop("checked",true);
+            treeGridCheckbox_s(ts);
+        }
         target.openAll = function(){
             var childs = target.find('[name=child]');
             for(var i = 0; i < childs.length; i++){
@@ -91,6 +185,18 @@
                 $(childs[i]).css("display","none");
             }
         }
+        target.getSelectedAll = function(){//获取所有勾选的
+            var ids = [];
+            var trs = target.find('tbody').find('tr');
+            for(var i = 0; i< trs.length; i++){
+                var obj = $(trs[i]);
+                var input = obj.find("input[type='checkbox']");
+                if(input.prop("checked")){
+                    ids.push(obj.attr("nodeId"));
+                }
+            }
+            return ids;
+        }
         target.createBody = function(data){
             //构造表体
             var tbody = $('<tbody></tbody>');
@@ -100,22 +206,35 @@
             }
             var rootNodeAndNowData = target.getRootNodes(data);
             var timestamp1 = new Date().getTime();
+            target.selectedIds = [];
             $.each(rootNodeAndNowData[0], function (i, value) {//加载顶级节点
-                var tr = $('<tr></tr>');
+                var tr = $('<tr nodeId="'+value[options.id]+'"></tr>');
                 tr.addClass('treegrid-' + value[options.id]);
                 tr.addClass("treegrid-expanded");
                 for (var j = 0; j < options.columns.length; j++) {
                     var td = $('<td></td>');
+                    var vl = value[options.columns[j].field];
                     if(j == 0 && value["child"]){
                         td.append('<span class="treegrid-expander treegrid-expander-collapsed" onclick="treeGridClickFun(this,\''+value[options.id]+'\',true)"></span>');
                     }
-                    td.append(value[options.columns[j].field]);
+                    if(j == 0 && options.selected) {//如果开启了勾选,则生成单选框并赋予点击事件
+                        var isselected = value["isselected"];
+                        if(isselected){
+                            target.selectedIds.push(value[options.id]);
+                        }
+                        var ckx = $('<input type="checkbox" onclick="treeGridCheckbox(this,\''+value[options.id]+'\')">');
+                        td.append(ckx);
+                    }
+                    td.append(vl);
                     tr.append(td);
                 };
                 tbody.append(tr);
                 target.getChildNodes(rootNodeAndNowData[1], value, tbody,0);//去递归加载子节点,使用新的数据结构
             });
             target.append(tbody);
+            for(var i = 0; i< target.selectedIds.length; i++){
+                target.setSelected(target.selectedIds[i]);
+            }
             var timestamp2 = new Date().getTime();
             console.log("组装耗时:" + (timestamp2 - timestamp1));
             //target.treegrid();
